bcb4cfb84208bf450b63a15c85fa722b0a66dd02
Rumit Tandukar <rumit0270@gmail.com>
2020-07-07 12:12:55 +0545

    add webpack config to handles css and react

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -10,6 +10,24 @@ const config = {
     // define the output filename
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   mode: 'none',
 };
 
